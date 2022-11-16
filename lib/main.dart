85ea31b6bcf84fef1995a4c2c02c1d5563694861
kisankang <rkdrltks@gmail.com>
2022-11-16 23:44:14 +0800

    initial feature complete

--- lib/main.dart
+++ lib/main.dart
@@ -1,115 +1,194 @@
+import 'dart:ffi';
+import 'dart:io';
+import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
+import 'package:path/path.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:math' as math;
 
 void main() {
-  runApp(const MyApp());
+  runApp(const MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: TakePhotoPage(),
+  ));
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class TakePhotoPage extends StatefulWidget {
+  const TakePhotoPage({super.key});
 
-  // This widget is the root of your application.
   @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+  State<TakePhotoPage> createState() => _TakePhotoPageState();
+}
+
+class _TakePhotoPageState extends State<TakePhotoPage> {
+  bool _cameraInitialized = false;
+  late CameraController _cameraController;
+
+  @override
+  void initState() {
+    // 화면에 처음 진입할 때 카메라 사용을 준비 하도록 합니다.
+    super.initState();
+    readyToCamera();
   }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
+  void dispose() {
+    // 화면에서 벗어날 때 카메라 제어기를 위해 OS에게 할당 받은 리소스를 정리 합니다.
+    if (_cameraController != null) {
+      _cameraController.dispose();
+    }
+    super.dispose();
+  }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+  void readyToCamera() async {
+    // 사용할 수 있는 카메라 목록을 OS로부터 받아 옵니다.
+    final cameras = await availableCameras();
+    if (0 == cameras.length) {
+      print("not found any cameras");
+      return;
+    }
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+    late CameraDescription frontCamera;
+    for (var camera in cameras) {
+      // 저 같은 경우에는 전면 카메라를 사용해야 했기 때문에
+      // 아래와 같이 코딩하여 카메라를 찾았습니다.
+      if (camera.lensDirection == CameraLensDirection.front) {
+        frontCamera = camera;
+        break;
+      }
+    }
+
+    _cameraController = CameraController(
+        frontCamera, ResolutionPreset.max // 가장 높은 해상도의 기능을 쓸 수 있도록 합니다.
+        );
+    _cameraController.initialize().then((value) {
+      // 카메라 준비가 끝나면 카메라 미리보기를 보여주기 위해 앱 화면을 다시 그립니다.
+      setState(() => _cameraInitialized = true);
     });
   }
 
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+  _mainPage(context) {
+    return GestureDetector(
+        onTap: () async {
+          XFile file = await _cameraController.takePicture();
+
+          // 찍은 사진을 별도 화면으로 보여주기 위해 TakePhotoResultPage화면으로 이동 합니다.
+          // 이 때 이동 하려는 화면으로 사진의 경로를 전달 합니다.
+          // 참고로 TakePhotoResultPage는 Flutter에서 제공하는 클래스가 아니고
+          // 제가 만든 클래스 입니다.
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                  builder: (context) =>
+                      TakePhotoResultPage(imagePath: file.path)));
+        },
+        child: Stack(
+          children: [
+            SizedBox(
+                width: Helper(context).scaledScreenWidth(1),
+                height: Helper(context).scaledScreenHeight(1),
+                child: Transform(
+                    alignment: Alignment.center,
+                    transform: Matrix4.rotationY(math.pi),
+                    child: CameraPreview(_cameraController))),
+            Center(
+              child: Icon(
+                Icons.face_outlined,
+                size: Helper(context).scaledScreenWidth(1),
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Container(
+                alignment: Alignment.center,
+                height: 70,
+                width: Helper(context).scaledScreenWidth(1),
+                color: Constants.guideBoxColor,
+                child: const Text(
+                  '화면의 빈 영역, 아무 곳이나 터치하면\n화면 안에 담긴 모습이 촬영 됩니다.',
+                  style: TextStyle(
+                    color: Colors.white,
+                  ),
+                  textAlign: TextAlign.center,
+                ),
+              ),
             ),
           ],
+        ));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: _cameraInitialized
+            ? _mainPage(context)
+            : Center(
+                child: CircularProgressIndicator(
+                    backgroundColor: Constants.selectionColor,
+                    valueColor: AlwaysStoppedAnimation<Color>(
+                        Constants.selectionColor))));
+  }
+}
+
+class TakePhotoResultPage extends StatelessWidget {
+  const TakePhotoResultPage({required this.imagePath, super.key});
+  final String imagePath;
+
+  _guideBoxAtBottom(BuildContext context) {
+    return Container(
+      height: 70,
+      width: MediaQuery.of(context).size.width,
+      color: Constants.guideBoxColor,
+      alignment: Alignment.center,
+      child: const Text(
+        'this is guideBox',
+        style: TextStyle(
+          color: Colors.white,
         ),
+        textAlign: TextAlign.center,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+        floatingActionButton: FloatingActionButton(
+          child: const Icon(Icons.arrow_back),
+          onPressed: () => Navigator.pop(context),
+        ),
+        body: Stack(
+          children: [
+            SizedBox(
+                width: Helper(context).scaledScreenWidth(1),
+                height: Helper(context).scaledScreenHeight(1),
+                child: Image.file(File(imagePath), fit: BoxFit.fitHeight)),
+            Align(
+                alignment: Alignment.bottomCenter,
+                child: _guideBoxAtBottom(context))
+          ],
+        ));
+  }
+}
+
+class Constants {
+  static Color selectionColor = Colors.red;
+  static Color guideBoxColor = Colors.black.withOpacity(0.9);
+}
+
+class Helper {
+  BuildContext context;
+  Helper(this.context);
+  double scaledScreenWidth(double width) {
+    double result;
+    result = width * MediaQuery.of(context).size.width;
+    return result;
+  }
+
+  double scaledScreenHeight(double height) {
+    double result;
+    result = height * MediaQuery.of(context).size.height;
+    return result;
   }
 }
